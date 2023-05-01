4829fe3faf09b9570bcd00b55653f058cc6a7a30
Drew Pomatti <ampomatti@gmail.com>
2023-05-01 15:57:47 -0700

    built out lat/lng search component, fixed env variable access

--- src/components/FlightSearch.vue
+++ src/components/FlightSearch.vue
@@ -1,13 +1,29 @@
 <template>
-    <div class="flight-search">
-        <form>
-            <input />
-        </form>
-    </div>
+  <div class="flight-search">
+    <form @submit.prevent="submitForm">
+      <label for="NWLL">North-West Longitude and Latitude:</label>
+      <input type="text" id="NWLL" v-model="NWLL" required>
+      <label for="SWLL">South-West Longitude and Latitude:</label>
+      <input type="text" id="SWLL" v-model="SWLL" required>
+      <button type="submit">Search Flights</button>
+    </form>
+  </div>
 </template>
 
 <script>
-
+export default {
+  data() {
+    return {
+      NWLL: "",
+      SWLL: "",
+    };
+  },
+  methods: {
+    submitForm() {
+      this.$emit("searchFlights", this.NWLL, this.SWLL);
+    },
+  },
+};
 </script>
 
 <style>
