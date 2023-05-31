aabf365ad9aa33a3e508325e568e64b6daefd59a
Drew Pomatti <ampomatti@gmail.com>
2023-05-31 13:04:55 -0700

    map working, limited to mapping 500 flights at a time

--- src/components/FlightMap.vue
+++ src/components/FlightMap.vue
@@ -1,6 +1,6 @@
 <template>
   <GoogleMap :api-key="mapsApiKey" style="width: 100%; height: 500px" :center="center" :zoom="10">
-    <Marker :options="markerOptions" />
+    <Marker v-for="flight in flights" :key="flight.reg_number" :options="getMarkerOptions(flight)" />
   </GoogleMap>
 </template>
 
@@ -11,12 +11,22 @@ import { GoogleMap, Marker } from "vue3-google-map";
 
 export default defineComponent({
   components: { GoogleMap, Marker },
+  props: {
+    flights: {
+      type: Array
+    },
+  },
   setup() {
     const mapsApiKey = import.meta.env.VITE_APP_GOOGLE_MAPS_API_KEY;
     const center = { lat: 34.049247, lng: -118.054502 };
-    const markerOptions = { position: { lat: 34.14, lng: -118.04 }, label: "FL1189", title: "Aéromexico" }
-
-    return { mapsApiKey, center, markerOptions };
+    const getMarkerOptions = (flight) => {
+      return {
+        position: { lat: flight.lat, lng: flight.lng},
+        label: flight.flight_icao,
+        icon: "src/assets/airplaneIconSmall.png"
+      }
+    }
+    return { mapsApiKey, center, getMarkerOptions };
   },
 });
 </script>
