efa194eb36a387f26069c03a7b302e4d043dcbb8
Drew Pomatti <ampomatti@gmail.com>
2023-05-30 18:35:57 -0700

    marker edits before mapping custom icon markers to map

--- src/components/FlightMap.vue
+++ src/components/FlightMap.vue
@@ -1,22 +1,20 @@
 <template>
   <GoogleMap :api-key="mapsApiKey" style="width: 100%; height: 500px" :center="center" :zoom="10">
-    <CustomMarker :options="markerOptions">
-      <img src="src/assets/airplaneIconSmall.png" />
-    </CustomMarker>
+    <Marker :options="markerOptions" />
   </GoogleMap>
 </template>
 
 <script>
 import { defineComponent } from "vue";
-import { GoogleMap, CustomMarker } from "vue3-google-map";
+import { GoogleMap, Marker } from "vue3-google-map";
 
 
 export default defineComponent({
-  components: { GoogleMap, CustomMarker },
+  components: { GoogleMap, Marker },
   setup() {
     const mapsApiKey = import.meta.env.VITE_APP_GOOGLE_MAPS_API_KEY;
     const center = { lat: 34.049247, lng: -118.054502 };
-    const markerOptions = { postion: { lat: 34.14, lng: -118.04 }, label: "FL1189", title: "Aéromexico" }
+    const markerOptions = { position: { lat: 34.14, lng: -118.04 }, label: "FL1189", title: "Aéromexico" }
 
     return { mapsApiKey, center, markerOptions };
   },
