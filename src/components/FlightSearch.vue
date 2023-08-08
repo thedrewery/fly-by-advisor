47180603965889fe66c2f0ded64aa05928865dfb
Drew Pomatti <ampomatti@gmail.com>
2023-08-08 14:52:38 -0700

    added updated at feature

--- src/components/FlightSearch.vue
+++ src/components/FlightSearch.vue
@@ -22,8 +22,8 @@ export default {
       const [lat, lng] = this.LL.split(" ");
       const parsedLat = parseFloat(lat);
       const parsedLng = parseFloat(lng);
-      this.NWLL = `${(parsedLat + 0.1)}, ${parsedLng - 0.1}`;
-      this.SELL = `${(parsedLat - 0.1)}, ${parsedLng + 0.1}`;
+      this.NWLL = `${(parsedLat + 0.05)}, ${parsedLng - 0.05}`;
+      this.SELL = `${(parsedLat - 0.05)}, ${parsedLng + 0.05}`;
       console.log("NWLL", this.NWLL, "SELL", this.SELL, "LL", this.LL);
       this.$emit("searchFlights", this.NWLL, this.SELL, this.LL);
     },
